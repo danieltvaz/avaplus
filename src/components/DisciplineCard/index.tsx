e0121f92377b766e3de7d51f1d85cf98721dadbd
danieltvaz <danieltostes@live.com>
2022-03-04 19:35:23 -0300

    show only not expired activity

--- src/components/DisciplineCard/index.tsx
+++ src/components/DisciplineCard/index.tsx
@@ -1,25 +1,30 @@
-import { Activity, Discipline } from "../../types/Types";
+import { Discipline } from "../../types/Types";
 import PagerView from "react-native-pager-view";
 
-import { DisciplineCardContainer, DisciplineCardTitle } from "./styles";
+import { DisciplineCardContainer, DisciplineCardTitle, CardTitle, CardStatsWrapper, CardTitleWrapper } from "./styles";
 
 import ActivityCard from "../ActivityCard";
 import filterRelevantActivities from "../../utils/filterRelevantActivities";
 
 type DisciplineCardProps = {
   discipline: Discipline | undefined;
+  courseId: string;
 };
 
 const IGNORE_LIST = ["Aa1", "Aa2", "Aa3", "Aa4", "Ta1", "Ta2", "Ta3", "Ta4"];
 
-export default function DisciplineCard({ discipline }: DisciplineCardProps): JSX.Element {
+export default function DisciplineCard({ discipline, courseId }: DisciplineCardProps): JSX.Element {
   return (
     <DisciplineCardContainer style={{ elevation: 7 }}>
-      <DisciplineCardTitle>{discipline?.name}</DisciplineCardTitle>
+      <DisciplineCardTitle>
+        <CardTitleWrapper>
+          <CardTitle>{discipline?.name}</CardTitle>
+        </CardTitleWrapper>
+      </DisciplineCardTitle>
       <PagerView initialPage={0} style={{ flex: 1 }}>
-        {filterRelevantActivities(discipline?.activities)?.map((activity, index) =>
-          IGNORE_LIST.includes(activity.name) ? "" : <ActivityCard activity={activity} key={index} />
-        )}
+        {filterRelevantActivities(discipline?.activities)?.map((activity, index) => (
+          <ActivityCard activity={activity} key={index} />
+        ))}
       </PagerView>
     </DisciplineCardContainer>
   );
--- src/utils/filterRelevantActivities.ts
+++ src/utils/filterRelevantActivities.ts
@@ -1,8 +1,9 @@
 import { Activity } from "../types/Types";
+import expired from "./expired";
 
 const IGNORE_LIST = ["Aa1", "Aa2", "Aa3", "Aa4", "Ta1", "Ta2", "Ta3", "Ta4"];
 
 export default function filterRelevantActivities(activities: Activity[] | undefined): Activity[] | undefined {
-  const relevantActivities = activities?.filter((activity) => (IGNORE_LIST.includes(activity.name) ? false : activity));
+  const relevantActivities = activities?.filter((activity) => (IGNORE_LIST.includes(activity.name) || expired(activity.date.end) ? false : activity));
   return relevantActivities;
 }
