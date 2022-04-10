f66c79b83d7b14ead4894a6735a8f72b4a36a337
danieltvaz <danieltostes@live.com>
2022-04-10 12:48:29 -0300

    feat: Ignoring irrelevant activities

--- src/utils/filterRelevantActivities.ts
+++ src/utils/filterRelevantActivities.ts
@@ -1,7 +1,7 @@
 import { Activity } from "../types/Types";
 import expired from "./expired";
 
-const IGNORE_LIST = ["Aa1", "Aa2", "Aa3", "Aa4", "Ta1", "Ta2", "Ta3", "Ta4"];
+const IGNORE_LIST = ["Aa1", "Aa2", "Aa3", "Aa4", "Ta1", "Ta2", "Ta3", "Ta4", "Aap1", "Aap2", "Aap3", "Aap4", "Adg1", "Adg2", "Adg3", "Adg4", "Leitura1", "Leitura2"];
 
 export default function filterRelevantActivities(activities: Activity[] | undefined): Activity[] | undefined {
   const relevantActivities = activities?.filter((activity) => (IGNORE_LIST.includes(activity.name) || expired(activity.date.end) ? false : activity));
